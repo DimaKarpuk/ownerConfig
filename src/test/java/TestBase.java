ad908649ba3530b151434c00431f7533c9aebbca
DimaKarpuk <99805543+DimaKarpuk@users.noreply.github.com>
2024-07-15 11:06:04 +0300

    added remote properties

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,7 +1,11 @@
+import com.codeborne.selenide.Selenide;
 import config.ConfigData;
 import config.WebDriverConfig;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+
+import java.lang.module.Configuration;
 
 public class TestBase {
     private static final ConfigData configData =
@@ -13,5 +17,9 @@ public class TestBase {
     public static void beforeAll(){
         WebDriverConfig webDriverConfig = new WebDriverConfig(configData);
         webDriverConfig.configData();
+    }
+    @AfterEach
+    public void afterEach(){
+        Selenide.closeWebDriver();
     }
 }
